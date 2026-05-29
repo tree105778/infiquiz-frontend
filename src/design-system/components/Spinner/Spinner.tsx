50c3b526243d558ed6a8de3be2709031169cd659
tree105778 <bjyon0415@gmail.com>
2026-05-30 01:28:50 +0900

    feat(spinner): Spinner 컴포넌트 추가

---
+++ src/design-system/components/Spinner/Spinner.tsx
@@ -0,0 +1,19 @@
+import type { HTMLAttributes } from 'react';
+import {
+  type SpinnerRecipeVariants,
+  spinner,
+} from '@/design-system/components/Spinner/Spinner.css';
+import { cx } from '@/utils/cx';
+
+export type SpinnerProps = HTMLAttributes<HTMLSpanElement> &
+  SpinnerRecipeVariants;
+
+export function Spinner({ size, className, ...rest }: SpinnerProps) {
+  return (
+    <output
+      aria-label="Loading"
+      className={cx(spinner({ size }), className)}
+      {...rest}
+    />
+  );
+}
