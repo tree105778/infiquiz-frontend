8667d58f54d57ed9b0d24a074161a0d075c24f84
tree105778 <bjyon0415@gmail.com>
2026-05-30 01:28:50 +0900

    feat(skeleton): Skeleton 컴포넌트 추가

---
+++ src/design-system/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,26 @@
+import type { CSSProperties, HTMLAttributes } from 'react';
+import { skeleton } from '@/design-system/components/Skeleton/Skeleton.css';
+import { cx } from '@/utils/cx';
+
+export type SkeletonProps = HTMLAttributes<HTMLDivElement> & {
+  width?: CSSProperties['width'];
+  height?: CSSProperties['height'];
+  radius?: CSSProperties['borderRadius'];
+};
+
+export function Skeleton({
+  width,
+  height,
+  radius,
+  className,
+  style,
+  ...rest
+}: SkeletonProps) {
+  return (
+    <div
+      className={cx(skeleton, className)}
+      style={{ width, height, borderRadius: radius, ...style }}
+      {...rest}
+    />
+  );
+}
