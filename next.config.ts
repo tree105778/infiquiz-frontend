0574f0cb253f48bb4f52da05a256b5fdc46f47cf
tree105778 <bjyon0415@gmail.com>
2026-05-30 01:28:49 +0900

    chore: Vanilla Extract Next 플러그인 연결

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
+
+const withVanillaExtract = createVanillaExtractPlugin({
+  unstable_turbopack: { mode: 'auto' },
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
