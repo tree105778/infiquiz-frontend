3dec8186d4dc1d88e1c4e6791be79a0305e5fca4
tree105778 <bjyon0415@gmail.com>
2026-05-30 01:28:50 +0900

    feat(pill): Pill 컴포넌트 추가

---
+++ src/design-system/components/Pill/Pill.tsx
@@ -0,0 +1,12 @@
+import { forwardRef, type HTMLAttributes } from 'react';
+import { cx } from '@/utils/cx';
+import { type PillRecipeVariants, pill } from './Pill.css';
+
+export type PillProps = HTMLAttributes<HTMLSpanElement> & PillRecipeVariants;
+
+export const Pill = forwardRef<HTMLSpanElement, PillProps>(function Pill(
+  { tone, className, ...rest },
+  ref,
+) {
+  return <span ref={ref} className={cx(pill({ tone }), className)} {...rest} />;
+});
