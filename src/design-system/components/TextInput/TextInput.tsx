e796ae4e92e488520e99aa5ced34ec08f586af6c
tree105778 <bjyon0415@gmail.com>
2026-05-30 01:28:51 +0900

    feat(text-input): TextInput 컴포넌트 추가

---
+++ src/design-system/components/TextInput/TextInput.tsx
@@ -0,0 +1,11 @@
+import { forwardRef, type InputHTMLAttributes } from 'react';
+import { textInput } from '@/design-system/components/TextInput/TextInput.css';
+import { cx } from '@/utils/cx';
+
+export type TextInputProps = InputHTMLAttributes<HTMLInputElement>;
+
+export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+  function TextInput({ className, ...rest }, ref) {
+    return <input ref={ref} className={cx(textInput, className)} {...rest} />;
+  },
+);
