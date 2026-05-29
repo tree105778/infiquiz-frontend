0810145f1c1fae926c081b250764af08dbd2cefb
tree105778 <bjyon0415@gmail.com>
2026-05-30 01:28:50 +0900

    feat(select): Select 컴포넌트 추가

---
+++ src/design-system/components/Select/Select.tsx
@@ -0,0 +1,11 @@
+import { forwardRef, type SelectHTMLAttributes } from 'react';
+import { select } from '@/design-system/components/Select/Select.css';
+import { cx } from '@/utils/cx';
+
+export type SelectProps = SelectHTMLAttributes<HTMLSelectElement>;
+
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  function Select({ className, ...rest }, ref) {
+    return <select ref={ref} className={cx(select, className)} {...rest} />;
+  },
+);
