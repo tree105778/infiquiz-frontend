e888ae607dcee6c52dbc1abddbb5093a1c950da9
tree105778 <bjyon0415@gmail.com>
2026-06-03 22:26:55 +0900

    docs(skeleton): Storybook title 소문자화

--- src/design-system/components/Skeleton/Skeleton.stories.tsx
+++ src/design-system/components/Skeleton/Skeleton.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Skeleton } from './Skeleton';
 
 const meta = {
-  title: 'Components/Skeleton',
+  title: 'components/Skeleton',
   component: Skeleton,
   tags: ['autodocs'],
   args: {
