615fd83d187e0ccd7863c859db4325ac713ed8b0
tree105778 <bjyon0415@gmail.com>
2026-06-17 02:24:08 +0900

    feat(home): TopicList server component 추가

---
+++ src/app/_components/TopicList.tsx
@@ -0,0 +1,25 @@
+import { Card, Text } from '@/design-system';
+import { getTopics } from '@/lib/api';
+
+export default async function TopicList() {
+  const topics = await getTopics();
+
+  return (
+    <div className="m-auto max-w-[50vw] p-6">
+      <div className="grid grid-cols-4 gap-4">
+        {topics.map((t) => (
+          <Card
+            key={t.slug}
+            interactive
+            className="h-35 flex flex-col justify-end py-6 px-2"
+          >
+            <Text as="h2" variant="displayLg">
+              {t.nameKo}
+            </Text>
+            <Text tone="inkMute">{t.nameEn}</Text>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
