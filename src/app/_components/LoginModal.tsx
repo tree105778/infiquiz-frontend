836a399eed119f49a64ea5051df54492a1768b5d
tree105778 <bjyon0415@gmail.com>
2026-06-17 02:24:08 +0900

    refactor(login): LoginModal을 app components로 이동

--- src/app/_components/LoginModal.tsx
+++ src/app/_components/LoginModal.tsx
@@ -3,12 +3,14 @@ import { useRouter, useSelectedLayoutSegment } from 'next/navigation';
 import type { ReactNode } from 'react';
 import { Modal } from '@/design-system';
 
+const LOGIN_LAYOUT_SEGMENT = '(.)login';
+
 export default function LoginModal({ children }: { children: ReactNode }) {
   const isOpen = useSelectedLayoutSegment('modal');
   const router = useRouter();
   return (
     <Modal
-      open={isOpen === 'login'}
+      open={isOpen === LOGIN_LAYOUT_SEGMENT}
       onClose={() => {
         router.back();
       }}
