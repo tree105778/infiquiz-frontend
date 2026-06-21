f356223fcde6bb92e4c6526a864269035b68079b
tree105778 <bjyon0415@gmail.com>
2026-06-21 17:44:40 +0900

    feat(home): TopicList를 상세 카드 목록으로 변경

--- src/app/_components/TopicList.tsx
+++ src/app/_components/TopicList.tsx
@@ -1,25 +1,23 @@
-import { Card, Text } from '@/design-system';
+import { Button, Card, Text } from '@/design-system';
 import { getTopics } from '@/lib/api';
 
 export default async function TopicList() {
   const topics = await getTopics();
 
   return (
-    <div className="m-auto max-w-[50vw] p-6">
-      <div className="grid grid-cols-4 gap-4">
-        {topics.map((t) => (
-          <Card
-            key={t.slug}
-            interactive
-            className="h-35 flex flex-col justify-end py-6 px-2"
-          >
-            <Text as="h2" variant="displayLg">
-              {t.nameKo}
-            </Text>
-            <Text tone="inkMute">{t.nameEn}</Text>
-          </Card>
-        ))}
-      </div>
+    <div>
+      {topics.map((t) => (
+        <Card key={t.slug} className="p-6 mb-2">
+          <Text as="h2" variant="displayLg">
+            {t.nameKo}
+          </Text>
+          <Text tone="inkMute">{t.nameEn}</Text>
+          <Text variant="bodyLg">{t.description}</Text>
+          <Button size="lg" className="mt-2">
+            도전
+          </Button>
+        </Card>
+      ))}
     </div>
   );
 }
